ef5e1b41d92fb9c897e72565dddefa2bce2a784e
CIbarra310 <chasen.ibarra@mac.com>
2024-04-20 14:53:20 -0700

    mimetypes update
    mimetimes update in settings.py

--- on_the_day/settings.py
+++ on_the_day/settings.py
@@ -15,7 +15,7 @@ import mimetypes
 
 
 mimetypes.add_type("text/css", ".css", True)
-
+mimetypes.add_type("text/html", ".js", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
